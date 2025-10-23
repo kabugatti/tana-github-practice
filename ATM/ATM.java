ae4bf8b06780c9ae1443980fde313705006f3f8f
Patrick Murimi <kabugap02@gmail.com>
2025-10-24 02:33:41 +0300

    chore: update OptionMenu

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class ATM {
     public static void main(String[] args) throws Exception {
-        optionMenu optionMenu = new optionMenu();
+        OptionMenu optionMenu = new optionMenu();
         optionMenu.getLogin();
     }
 }
