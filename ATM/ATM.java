180217f34b74151816dcf28eaaa9284b5537ccd3
Patrick Murimi <kabugap02@gmail.com>
2025-10-24 02:31:24 +0300

    feat: add ATM project

---
+++ ATM/ATM.java
@@ -0,0 +1,10 @@
+package ATM;
+
+import java.io.IOException;
+
+public class ATM {
+    public static void main(String[] args) throws Exception {
+        optionMenu optionMenu = new optionMenu();
+        optionMenu.getLogin();
+    }
+}
